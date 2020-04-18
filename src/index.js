1155fc529e31de9c0df35c5d6017e260d59bcd0c
lurkshark <13933101+lurkshark@users.noreply.github.com>
2020-04-18 16:47:08 -0700

    Add rough server component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import Router from 'preact-router'
+import {Main, Gameplay, About} from './scenes'
 import './style';
 
-export default function Yatzy() {
-  return (<h1>Yatzy</h1>)
+export default function App() {
+  return (
+    <Router>
+      <Main path='/' />
+      <Gameplay path='/play' />
+      <About path='/about' />
+    </Router>
+  )
 }
---
+++ src/scenes/main.js
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <div class="wrapper">
+      <h1 class="title">Yatzy Deathmatch</h1>
+      <p><a href="/play">Play</a></p>
+      <p><a href="/about">About</a></p>
+    </div>
+  )
+}
+
