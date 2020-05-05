92d9400cd1acb7e3d3f63f512ee8f8b0bb5dafc3
lurkshark <13933101+lurkshark@users.noreply.github.com>
2020-05-05 12:50:27 -0700

    Add a precaching service worker

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,8 @@
 import Yatzy from "./yatzy"
 import './style.css'
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+}
+
 new Yatzy(window, document.body)
